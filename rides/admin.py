04f52ba3d546cd6308e0b39d02efaa9a601fb981
Marcel bag <marcelshabani@gmail.com>
2025-03-13 17:48:09 +0200

    updating admin

--- rides/admin.py
+++ rides/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import Ride
+from .models import CustomUser
 
 admin.site.register(Ride)
+admin.site.register(CustomUser)
